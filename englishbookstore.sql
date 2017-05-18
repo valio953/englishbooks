fb486c3e9f13f0e39f26cdbbb59975055c4139b9
Valentin <valio953@gmail.com>
2017-05-18 03:40:54 +0200

    db dump

---
+++ englishbookstore.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2017 at 03:39 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `englishbookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_gr_id` int(11) NOT NULL,
+  `author_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors_books`
+--
+
+CREATE TABLE `authors_books` (
+  `ab_id` int(11) NOT NULL,
+  `ab_author` int(11) NOT NULL,
+  `ab_book` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_isbn` bigint(20) NOT NULL,
+  `book_quantity` int(11) NOT NULL DEFAULT '1',
+  `book_category` int(11) NOT NULL,
+  `book_title` char(100) NOT NULL,
+  `book_description` text,
+  `book_img` text,
+  `book_reserved` enum('yes','no') NOT NULL DEFAULT 'no',
+  `book_reservation_date` datetime DEFAULT NULL,
+  `book_reservation_name` varchar(100) DEFAULT NULL,
+  `book_price` decimal(10,2) DEFAULT NULL,
+  `book_regdate` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_gr_id`);
+
+--
+-- Indexes for table `authors_books`
+--
+ALTER TABLE `authors_books`
+  ADD PRIMARY KEY (`ab_id`),
+  ADD KEY `ab_author` (`ab_author`),
+  ADD KEY `ab_book` (`ab_book`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_isbn`),
+  ADD KEY `book_reserved` (`book_reserved`),
+  ADD KEY `book_regdate` (`book_regdate`),
+  ADD KEY `book_title` (`book_title`),
+  ADD KEY `book_category` (`book_category`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `authors_books`
+--
+ALTER TABLE `authors_books`
+  MODIFY `ab_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
